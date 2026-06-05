e02d7574966a1f028fbb5ea7f7c43ebc84ce0210
Cyber Knight <cyberknight755@gmail.com>
2026-06-06 00:49:27 +0800

    cancunf: Sync with Device Tree
    - Remove Moto shutdown logging script.
    
    Change-Id: I3cee1c2e0757c95bf8f12389e93e05cd5183bd86
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- cancunf-vendor.mk
+++ cancunf-vendor.mk
@@ -1769,7 +1769,6 @@ PRODUCT_PACKAGES += \
     init.mmi.backup.trustlet \
     init.mmi.block_perm \
     init.mmi.boot \
-    init.mmi.shutdown \
     init.mmi.touch \
     kpgather \
     kpreadwrite \
